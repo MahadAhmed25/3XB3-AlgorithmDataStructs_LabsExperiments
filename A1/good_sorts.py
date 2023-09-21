e7ee643425b5d59deb9b921619ee4eac793124b1
Saad Salman <28.saadsalman@gmail.com>
2023-09-20 22:34:45 -0400

    Experiment 4 Done

--- A1/good_sorts.py
+++ A1/good_sorts.py
@@ -9,6 +9,14 @@ In contains traditional implementations for:
 
 Author: Vincent Maccio
 """
+import math
+import timeit
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+def create_random_list(length, max_value):
+    return [random.randint(0, max_value) for _ in range(length)]
 
 # ************ Quick Sort ************
 def quicksort(L):
@@ -146,4 +154,67 @@ class Heap:
         return s
 
 # *************************************
+
+def experiment(numberOfRuns, numberOfElements):
+    total1 = 0
+    total2 = 0
+    total3 = 0
+    for i in range(numberOfRuns):
+        randomList = create_random_list(numberOfElements, 10000)
+
+        start = timeit.default_timer()
+        quicksort(randomList)
+        total1 += timeit.default_timer() - start
+
+        start = timeit.default_timer()
+        mergesort(randomList)
+        total2 += timeit.default_timer() - start
+
+        start = timeit.default_timer()
+        heapsort(randomList)
+        total3 += timeit.default_timer() - start
     
+    x1=total1/numberOfRuns
+    x2= total2/numberOfRuns
+    x3= total3/numberOfRuns
+    
+    print("Quick sort -- listLength: ", numberOfElements, " time: ", x1, "sec")
+    print("Merge sort -- listLength: ", numberOfElements, " time: ", x2, "sec")
+    print("Heap sort -- listLength: ", numberOfElements, " time: ", x3, "sec")
+    print("================================================================")
+    return x1,x2,x3
+
+def run_experiment2():
+
+    q1,m1,h1 = experiment(100,10)
+    q2,m2,h2 = experiment(100,100)
+    q3,m3,h3 = experiment(100,1000)
+    q4,m4,h4 = experiment(100,5000)
+    q5,m5,h5 = experiment(100,10000)
+
+    q=[q1,q2,q3,q4,q5]
+    m=[m1,m2,m3,m4,m5]
+    h=[h1,h2,h3,h4,h5]
+    y=[10,100,1000,5000,10000]
+
+    fig, ax = plt.subplots()
+    ax.scatter(q, y)
+    ax.scatter(m, y, color='r')
+    ax.scatter(h, y, color='g')
+
+    plt.plot(q, y, label = "Quick sort")
+    plt.plot(m, y, label = "Merge sort")
+    plt.plot(h, y, label = "Heap sort")
+
+    ax.set_xlabel('Time (seconds)')
+    ax.set_ylabel('Length of List')
+    ax.legend()
+    ax.set_title('List length vs Time Displaying for ten runs')
+
+
+    plt.show()
+
+def main():
+    run_experiment2()
+
+main()
