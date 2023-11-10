dddc07e95fb439d93e316392cb4d64480c589298
AliZia3 <alizia0217@gmail.com>
2023-11-09 23:04:54 -0500

    implemented num_of_wc_runs

--- A3/brick_lab.py
+++ A3/brick_lab.py
@@ -1,31 +1,47 @@
-import math
+# Base Cases
+# if m = 1, then num_of_wc_runs(m, n) = n (where num_of_wc_runs(m, n) is min num of tests needed for m bricks and n force settings)
+# if n = 1, then num_of_wc_runs(m, n) = 1
+# if n = 0 or m = 0, then num_of_wc_runs(m, n) = 0
 
-'''
-Recursion:
+# Recursive (Finding minimum number of attempts needed to determine the highest safe force setting)
+# num_of_wc_runs(m,n) = min(max(num_of_wc_runs(m-1, k-1), num_of_wc_runs(m, n-k))) for k in range(1, j+1)
 
-n := range of forces from 1 to n
-m := number of bricks
+def num_of_wc_runs(n, m):
+    # n - number of force settings, m - number of bricks
 
-let brickTest(m,n) := min # of tests to find threshold force k* where brick breaks
+    # make the table holding results fo subproblems
+    brickTest = [[0 for _ in range(n+1)] for _ in range(m+1)]
+ 
+    # Base case: if 1 brick, we test each force setting 
+    for i in range(1, m + 1):
+        brickTest[i][1] = 1
+        brickTest[i][0] = 0
+ 
+    for j in range(1, n + 1):
+        brickTest[1][j] = j
+ 
+    # Fill table
+    for i in range(2, m + 1):
+        for j in range(2, n + 1):
+            brickTest[i][j] = float('inf')
+            # considering all possible first moves (testing brick at each force setting k)
+            # 1. brick breaks, we have 1 less brick and k-1 remaining force settings to test
+            # 2. brick doesnt break and we still have all bricks and j-k force settings left to test
+            for k in range(1, j + 1):
+                wc = 1 + max(brickTest[i-1][k-1], brickTest[i][j-k])
 
-if we have 1 brick left we must perform linear search to find k* therefore worst case would be O(n-1):
-brickTest(1,n) = n - 1
+                # if this move has lower worst case than current best we needa update it
+                if wc < brickTest[i][j]:
+                    brickTest[i][j] = wc
+ 
+    # last element in tbae is min num of test needed for m bricks and n force settings
+    return brickTest[m][n]
 
-if we have as many bricks as searches for the binary search (ie. m >= log2(n) + 1) then we can perform binary search and find k*:
-brickTest(m,n) = log2(n)   if m >= log2(n) + 1
-'''
 
-def brickTest_rec(m,n):
-    if m < 1 or n < 1:
-        return 0
-    
-    if m == 1:
-        return n - 1
+def next_setting(n, m):
+    pass
 
-    if m >= math.log(n,2) + 1:
-        return math.log(n,2)
 
-n = 100
-m = 2
-print("Minimum Tests Required To Find Threshold Force For Brick Breaking: ", brickTest_rec(m,n))
-        
+
+print(num_of_wc_runs(100, 2))
+# print(next_setting(100, 2))
