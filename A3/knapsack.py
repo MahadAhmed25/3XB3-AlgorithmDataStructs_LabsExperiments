23ce94d0a173298289e3da08e2c45727a3b5606f
Saad Salman <28.saadsalman@gmail.com>
2023-11-09 22:01:42 -0500

    Top-Down Approach Done

--- A3/knapsack.py
+++ A3/knapsack.py
@@ -46,13 +46,51 @@ def ks_bottom_up(sack, capacity):
     return matrix[len(sack)][capacity]
 
 def ks_top_down(sack, capacity):
+    sp = [[-1 for j in range(capacity + 1)] for i in range(len(sack) + 1)]
+    
+    for i in range(len(sack) + 1):
+        sp[i][0] =0
+    
+    for j in range(capacity + 1):
+        sp[0][j] =0
+
+    top_down_aux(sack, len(sack), capacity, sp)
+    
+    print_matrix(sp)
     return True
+
+def top_down_aux(sack, i, j, sp):
+    if sack[i-1][0] > j:
+        if sp[i - 1][ j] == -1:
+            top_down_aux(sack, i - 1, j, sp)
+        sp[i][j] = sp[i - 1][j] 
+    
+    else:
+        if sp[i - 1][ j] == -1:
+            top_down_aux(sack, i - 1, j, sp)
+            
+        if sp[i - 1][j - sack[i - 1][0]] == -1:
+            top_down_aux(sack, i - 1, j - sack[i-1][0], sp)
+
+        sp[i][j] = max(sp[i - 1][j], sp[i - 1][j - sack[i-1][0]] + sack[i-1][1])
+
+    
+
     
     
     
 #Example code to create a knapsack
 x = create_knapsack(10, 1, 10, 1, 10)
-print(x)
+#print(x)
+#print(x[2][1])
 
-y = ks_bottom_up(x, 20)
+"""print_matrix(sp)
+sp[3][4]=4
+print(sp[3][4])
+print_matrix(sp)"""
+
+t = ks_bottom_up(x, 20)
+print(t)
+
+y = ks_top_down(x, 20)
 print(y)
